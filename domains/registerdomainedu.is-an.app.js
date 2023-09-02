c4022d86c91e531c4983b7d6de77f00deca422e3
hungnemg <136260151+hungnemg@users.noreply.github.com>
2023-09-02 20:36:05 +0700

    Create registerdomainedu.is-an.app.js

---
+++ domains/registerdomainedu.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Education',
+  domain: 'is-an.app',
+  subdomain: 'registerdomainedu',
+  owner: {
+    repo: 'https://github.com/yopremium21/abhirockz',
+    email: 'uhungne@gmail.com',
+  },
+  record: {
+    CNAME: 'admin.github.io',
+  },
+  proxy: false,
+})
